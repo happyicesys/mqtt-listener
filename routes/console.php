51a07bb00a00c950503d39beb7c26f36a1d74b01
happyicesys <brianlee@happyice.com.my>
2025-02-10 15:44:47 +0800

    add scheduler delete more than 7 days

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Scedule::command('remove:vend-data-more-than-seven-days')->daily();
+Schedule::command('remove:vend-data-more-than-seven-days')->daily();
